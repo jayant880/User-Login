601f7287e722aed094f8f5abef476c5ba3ce9ef8
Jayant <188503548+jayant880@users.noreply.github.com>
2025-08-24 09:51:40 +0000

    Form validation

--- src/Components/UserForm.tsx
+++ src/Components/UserForm.tsx
@@ -1,43 +1,69 @@
-import { useState, type FormEvent } from 'react';
+import { useEffect, useState, type FormEvent } from 'react';
+
 const UserForm = () => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<boolean>(false);
+    const [usernameError, setUsernameError] = useState<string>('');
+    const [passwordError, setPasswordError] = useState<string>('');
+
+    useEffect(() => {
+        setUsernameError('');
+        setPasswordError('');
+        const usernameErrors: string[] = [];
+        const passwordErrors: string[] = [];
+        if (username === '') { usernameErrors.push('Username cannot be empty'); }
+        if (username.length < 4) { usernameErrors.push('Username cannot be less than 4 character'); }
+        if (password === '') { passwordErrors.push('Password cannot be empty'); }
+        if (password.length < 6) { passwordErrors.push('password should be greater than 6 charcter'); }
+        if (!(/[^a-zA-Z0-9\s]/.test(password))) { passwordErrors.push('password should include special symbol'); }
+        setUsernameError(usernameErrors.join(' / '));
+        setPasswordError(passwordErrors.join(' / '));
+        if (usernameErrors.length !== 0 || passwordErrors.length !== 0) setError(true)
+        else setError(false)
+    }, [username, password])
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
-        console.log(username, password)
+        if (!error) console.log(username, password);
     }
     return (
         <div className='max-w-2xl'>
             <form onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-4">
-                    <div className='border rounded-2xl m-2 px-4 py-2 bg-gray-100 flex items-center'>
-                        <label
-                            htmlFor="username"
-                            className='text-lg '
-                        >Username :</label>
-                        <input
-                            type="text"
-                            value={username}
-                            name='username'
-                            placeholder='Enter Username'
-                            onChange={e => setUsername(e.target.value)}
-                            className='px-3 m-2 py-2 text-xl border-none focus:outline-blue-300 flex-1 focus:outline-2 rounded'
-                        />
+                    <div className='border rounded-2xl m-2 px-4 py-2 bg-gray-100'>
+                        <div className='flex items-center'>
+                            <label
+                                htmlFor="username"
+                                className='text-lg font-bold'
+                            >Username :</label>
+                            <input
+                                type="text"
+                                value={username}
+                                name='username'
+                                placeholder='Enter Username'
+                                onChange={e => setUsername(e.target.value)}
+                                className='px-3 m-2 py-2 text-xl border-none bg-white focus:outline-blue-300 flex-1 focus:outline-2 rounded-xl'
+                            />
+                        </div>
+                        <span className='text-xl text-red-500 flex flex-1 justify-center bg-amber-100 rounded capitalize'>{usernameError}</span>
                     </div>
-                    <div className='border rounded-2xl m-2 px-4 py-2 bg-gray-100 flex items-center'>
-                        <label
-                            htmlFor="password"
-                            className='text-lg '
-                        >Password :</label>
-                        <input
-                            type="password"
-                            value={password}
-                            name='password'
-                            placeholder='Enter Password'
-                            onChange={e => setUsername(e.target.value)}
-                            className='px-3 m-2 py-2 text-xl border-none bg-white focus:outline-blue-300 flex-1 focus:outline-2 rounded'
-                        />
+                    <div className='border rounded-2xl m-2 px-4 py-2 bg-gray-100'>
+                        <div className="flex items-center">
+                            <label
+                                htmlFor="password"
+                                className='text-lg font-bold'
+                            >Password : </label>
+                            <input
+                                type="password"
+                                value={password}
+                                name='password'
+                                placeholder='Enter Password'
+                                onChange={e => setPassword(e.target.value)}
+                                className='px-3 m-2 py-2 text-xl border-none bg-white focus:outline-blue-300 flex-1 focus:outline-2 rounded-xl'
+                            />
+                        </div>
+                        <span className='text-xl text-red-500 flex flex-1 justify-center bg-amber-100 rounded capitalize'>{passwordError}</span>
                     </div>
                     <div className='flex items-center'>
                         <button
