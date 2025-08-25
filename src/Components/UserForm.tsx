33942e392821650b488498967f5989ee339248f0
Jayant <188503548+jayant880@users.noreply.github.com>
2025-08-25 12:21:28 +0000

    User FeedBack

--- src/Components/UserForm.tsx
+++ src/Components/UserForm.tsx
@@ -6,6 +6,7 @@ const UserForm = () => {
     const [error, setError] = useState<boolean>(false);
     const [usernameError, setUsernameError] = useState<string>('');
     const [passwordError, setPasswordError] = useState<string>('');
+    const [sucess, setSucess] = useState<string>('');
 
     useEffect(() => {
         setUsernameError('');
@@ -20,15 +21,24 @@ const UserForm = () => {
         setUsernameError(usernameErrors.join(' / '));
         setPasswordError(passwordErrors.join(' / '));
         if (usernameErrors.length !== 0 || passwordErrors.length !== 0) setError(true)
-        else setError(false)
+        else {
+            setError(false)
+            setSucess('');
+        }
     }, [username, password])
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
-        if (!error) console.log(username, password);
+        if (!error) {
+            console.log(username, password);
+            setSucess('Login Successful');
+        }
     }
     return (
         <div className='max-w-2xl'>
+            {sucess.length > 0 &&
+                <span className='absolute top-3 right-1.5 border-2 rounded-2xl px-4 py-3 m-4 text-3xl bg-green-500 text-white font-bold animate-bounce'>{sucess}</span>
+            }
             <form onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-4">
                     <div className='border rounded-2xl m-2 px-4 py-2 bg-gray-100'>
@@ -43,7 +53,7 @@ const UserForm = () => {
                                 name='username'
                                 placeholder='Enter Username'
                                 onChange={e => setUsername(e.target.value)}
-                                className='px-3 m-2 py-2 text-xl border-none bg-white focus:outline-blue-300 flex-1 focus:outline-2 rounded-xl'
+                                className={`px-3 m-2 py-2 text-xl bg-white border-2 focus:outline-blue-300 flex-1 focus:outline-2 rounded-xl ${usernameError === '' ? 'border-green-500' : 'border-red-500'}`}
                             />
                         </div>
                         <span className='text-xl text-red-500 flex flex-1 justify-center bg-amber-100 rounded capitalize'>{usernameError}</span>
@@ -60,7 +70,7 @@ const UserForm = () => {
                                 name='password'
                                 placeholder='Enter Password'
                                 onChange={e => setPassword(e.target.value)}
-                                className='px-3 m-2 py-2 text-xl border-none bg-white focus:outline-blue-300 flex-1 focus:outline-2 rounded-xl'
+                                className={`px-3 m-2 py-2 text-xl bg-white border-2 focus:outline-blue-300 flex-1 focus:outline-2 rounded-xl ${passwordError === '' ? 'border-green-500' : 'border-red-500'}`}
                             />
                         </div>
                         <span className='text-xl text-red-500 flex flex-1 justify-center bg-amber-100 rounded capitalize'>{passwordError}</span>
